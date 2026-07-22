7ae9c0d6010d796ef9f00cca32b7c50eb1a2171c
Raulspl2020 <raulspl2020@gmail.com>
2026-07-22 17:28:51 -0500

    Actaulizacion de descuentos en Especializaciones v3

--- src/modules/invoice/repositories/package.repository.ts
+++ src/modules/invoice/repositories/package.repository.ts
@@ -27,4 +27,19 @@ export class PackageRepository extends Repository<Package> {
       .where('pk.codigo = :packageCode', { packageCode })
       .getOne();
   }
+
+  findConceptsByProgramName(programName: string, categoryId = 1) {
+    return this.createQueryBuilder('pk')
+      .select('pk')
+      .innerJoinAndSelect('pk.packageDetail', 'pkd')
+      .innerJoinAndSelect('pkd.concept', 'con')
+      .innerJoinAndSelect('pk.config', 'pkc')
+      .where('pk.categoriaId = :categoryId', { categoryId })
+      .andWhere('UPPER(TRIM(pk.descripcion)) = UPPER(TRIM(:programName))', {
+        programName,
+      })
+      .orderBy('pk.id', 'ASC')
+      .addOrderBy('pkd.id', 'ASC')
+      .getMany();
+  }
 }
